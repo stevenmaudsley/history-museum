a200777ae1c13b5f0e2266a883df74f0a45f652d
“Steven <steven.maudsley@ankorstore.com>
2023-04-25 16:30:35 +0100

    Remove boilerplate and import test files

---
+++ src/components/MuseumHighlight.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="museum-highlight">
+    <!-- Display the available information for the highlight -->
+    <h1>Museum Highlight</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MuseumHighlight",
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {
+    newsDate() {
+      return null;
+      // Highlight's news item date
+    },
+  },
+  methods: {},
+  created() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.museum-highlight {
+}
+</style>
---
+++ src/components/SpacePage.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="space-page">
+    <h1 class="space-page__title">Space</h1>
+
+    <!-- Add the museum highlight cards based on the data provided below -->
+    <MuseumHighlight />
+  </div>
+</template>
+
+<script>
+import MuseumHighlight from "./MuseumHighlight";
+
+export default {
+  name: "SpacePage",
+  components: {
+    MuseumHighlight,
+  },
+  mixins: [],
+  props: {},
+  data() {
+    return {
+      spaceHighlights: [
+        {
+          date: "2020-04-20 12:20:00",
+          description:
+            "Asteroids are minor planets, especially of the inner Solar System. Larger asteroids have also been called planetoids.",
+          id: 1,
+          image: "",
+          name: "Asteroids",
+        },
+        {
+          date: "2020-05-20 15:50:00",
+          description:
+            "A comet is an icy, small Solar System body that, when passing close to the Sun, warms and begins to release gases, a process called outgassing.",
+          id: 9,
+          image: "",
+          name: "Comets",
+        },
+        {
+          date: "2020-05-01 9:22:00",
+          description:
+            "The term planet is ancient, with ties to history, astrology, science, mythology, and religion.",
+          id: 7,
+          image: "",
+          name: "Planets",
+          news: {
+            date: "2020-08-18 18:00:00",
+            title: "Attend our talk about Jupiter with Dr. Hogarth",
+          },
+          quiz: "https://planetquiz.space",
+        },
+        {
+          date: "2020-07-05 4:10:00",
+          description:
+            "A meteor shower is a celestial event in which a number of meteors are observed to radiate, or originate, from one point in the night sky.",
+          id: 12,
+          image: "",
+          name: "Meteor showers",
+          news: {
+            title: "The Lyrids will peak at on April 21-22 2021, at night",
+          },
+        },
+      ],
+      spacePartners: {
+        observatory: {
+          createdAt: "2020-06-01 11:45:00",
+          info: "The Mauna Kea Observatories (MKO) are a number of independent astronomical research facilities and large telescope observatories that are located at the summit of Mauna Kea on the Big Island of Hawaiʻi, United States.",
+          image: "",
+          name: "Mauna Kea Observatories",
+        },
+      },
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.space-page {
+  &__title {
+    color: #2c3791;
+    font-size: 24px;
+    font-weight: 600;
+  }
+}
+</style>
